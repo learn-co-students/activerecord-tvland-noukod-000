bf729293e900edd50d1087004effb658e842b8f1
Gilbert Torchon <gilbert@noukod.com>
2018-12-05 19:55:46 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
+require 'pry'
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors,through: :characters
+  belongs_to :network
+
+  # def build_network(call_letters:nil)
+  #   network=Network.create(:call_letters=>call_letters)
+  #   #binding.pry
+  #   self.network_id=network.id
+  #   network
+  # end
 end
